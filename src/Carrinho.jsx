a299bc33332284bb195e0e09c2c5f6c63675212f
daviguerra05 <davi@passanha.com.br>
2023-10-03 15:47:05 -0300

    unica coisa pior que react é bolovo

--- src/Carrinho.jsx
+++ src/Carrinho.jsx
@@ -1,11 +1,26 @@
 import Button from 'react-bootstrap/Button';
-
+import '../src/css/carrinho.css'
 function Carrinho(){
+    const add = ()=>{
+        const contador = document.getElementById('contador')
+        let itens = parseInt(contador.innerHTML) + 1
+        contador.innerHTML = itens
+    }
+    const remove = ()=>{
+        const contador = document.getElementById('contador')
+        let itens = parseInt(contador.innerHTML)
+        if (itens > 0){
+            contador.innerHTML = itens - 1
+        }
+    }
     return(
         <div>
             <h1>Carrinho</h1>
+            <div id='carrinho'></div>
+            <div id='contador'>0</div>
             <>
-                <Button variant="dark">Adicionar Item</Button>
+                <Button variant="dark" onClick={add}>Adicionar Item</Button>
+                <Button variant="dark" onClick={remove}>Remover Item</Button>
             </>
         </div>
     )
--- src/Outlet.jsx
+++ src/Outlet.jsx
@@ -1,6 +1,46 @@
+import Carousel from 'react-bootstrap/Carousel';
+import '../src/css/carousel.css'
+
 function Outlet(){
     return(
-        <h1>Outlet</h1>
+        <Carousel className='carousel_div' data-bs-theme="dark">
+        <Carousel.Item>
+            <img
+            className="d-block w-100"
+            src="src\assets\product-1.jpg"
+            alt="First slide"
+            height={'500px'}
+            />
+            <Carousel.Caption className='c_cap'>
+            <h5>Fone de Ouvido</h5>
+            <p>Relógio Masculino de luxo Vivara</p>
+            </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+            <img
+            className="d-block w-100"
+            src="src\assets\product-7.jpg"
+            alt="Second slide"
+            />
+            <Carousel.Caption>
+            <h5>Sapato</h5>
+            <p>Sapato Social Masculino de couro</p>
+            </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+            <img
+            className="d-block w-100"
+            src="src\assets\cat-img-2.jpg"
+            alt="Third slide"
+            />
+            <Carousel.Caption>
+            <h5>Camiseta Branca</h5>
+            <p>
+                Camiseta Branca Masculina Simples
+            </p>
+            </Carousel.Caption>
+        </Carousel.Item>
+        </Carousel>
     )
 }
 
